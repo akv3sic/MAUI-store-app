86a5ea403c0f65ce8ebc7c836f65d5824b3f9825
akv3sic <antekvesic2@gmail.com>
2023-09-14 20:34:42 +0200

    #15 Remove/refactor app title bar

--- MauiStoreApp/MauiProgram.cs
+++ MauiStoreApp/MauiProgram.cs
@@ -17,6 +17,7 @@ namespace MauiStoreApp
                 {
                     fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
                     fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+                    fonts.AddFont("astore-eczar-semi-bold.ttf", "astore-eczar-semi-bold");
                 }).UseMauiCommunityToolkit();
 
             builder.Services.AddSingleton<BaseService>();
