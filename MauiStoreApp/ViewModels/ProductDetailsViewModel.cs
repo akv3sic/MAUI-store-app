446b6276c7e63b8aa725c4d336d3d0343976a78a
akv3sic <antekvesic2@gmail.com>
2023-09-19 23:29:24 +0200

    #24 Implement cross-sell functionalities

--- MauiStoreApp/ViewModels/ProductDetailsViewModel.cs
+++ MauiStoreApp/ViewModels/ProductDetailsViewModel.cs
@@ -3,6 +3,8 @@ using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
 using MauiStoreApp.Models;
 using MauiStoreApp.Services;
+using MauiStoreApp.Views;
+using System.Collections.ObjectModel;
 using System.Diagnostics;
 
 namespace MauiStoreApp.ViewModels
@@ -18,13 +20,20 @@ namespace MauiStoreApp.ViewModels
             _productService = productService;
         }
 
+        public ProductDetailsViewModel()
+        {
+        }   
+
         [ObservableProperty]
         Product product;
+
+        public ObservableCollection<Product> CrossSellProducts { get; private set; } = new ObservableCollection<Product>();
 
         [RelayCommand]
         public async Task Init()
         { 
             await GetProductByIdAsync();
+            await GetCrossSellProductsAsync();
         }
 
         private async Task GetProductByIdAsync()
@@ -48,6 +57,54 @@ namespace MauiStoreApp.ViewModels
             {
                 IsBusy = false;
             }
-        }   
+        }
+
+        private async Task GetCrossSellProductsAsync()
+        {
+            if (IsBusy || Product == null)
+                return;
+
+            try
+            {
+                IsBusy = true;
+                var crossSellProducts = await _productService.GetProductsByCategoryAsync(Product.Category);
+                CrossSellProducts.Clear();
+                foreach (var crossSellProduct in crossSellProducts)
+                {
+                    if (crossSellProduct.Id != Product.Id) // exclude the current product
+                    {
+                        CrossSellProducts.Add(crossSellProduct);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine($"Unable to get cross-sell products: {ex.Message}");
+                await Shell.Current.DisplayAlert("Error!", ex.Message, "OK");
+            }
+            finally
+            {
+                IsBusy = false;
+            }
+        }
+
+        [RelayCommand]
+        private async Task ProductTapped(Product product)
+        {
+            IsBusy = true;
+
+            if (product == null)
+                return;
+
+            var navigationParameter = new Dictionary<string, object>
+            {
+                { "Product", product }
+            };
+
+
+            await Shell.Current.GoToAsync($"{nameof(ProductDetailsPage)}", true, navigationParameter);
+
+            IsBusy = false;
+        }
     }
 }
