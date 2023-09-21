1bbf079d8db9efda9bee5daa987ebb12bc6da1e3
akv3sic <antekvesic2@gmail.com>
2023-09-21 18:56:25 +0200

    #28 Handle the case when there is no internet connection

--- MauiStoreApp/Services/BaseService.cs
+++ MauiStoreApp/Services/BaseService.cs
@@ -1,4 +1,6 @@
-﻿namespace MauiStoreApp.Services
+﻿using System.Diagnostics;
+
+namespace MauiStoreApp.Services
 {
     public class BaseService
     {
@@ -14,13 +16,38 @@
 
         protected async Task<T> GetAsync<T>(string endpoint)
         {
-            var response = await _httpClient.GetAsync(endpoint);
+            NetworkAccess accessType = Connectivity.Current.NetworkAccess;
 
+            if (accessType != NetworkAccess.Internet)
+            {
+                if (Shell.Current != null)
+                {
+                    if (accessType == NetworkAccess.ConstrainedInternet)
+                        await Shell.Current.DisplayAlert("Greška!", "Internet veza je ograničena.", "U redu");
+                    else
+                        await Shell.Current.DisplayAlert("Greška!", "Internet veza nije dostupna.", "U redu");
+                }
+                return default;
+            }
+            else
+            {
+                try
+                {
+                    var response = await _httpClient.GetAsync(endpoint);
 
-            response.EnsureSuccessStatusCode();
+                    response.EnsureSuccessStatusCode();
 
-            var responseContent = await response.Content.ReadAsStringAsync();
-            return System.Text.Json.JsonSerializer.Deserialize<T>(responseContent);
+                    var responseContent = await response.Content.ReadAsStringAsync();
+                    return System.Text.Json.JsonSerializer.Deserialize<T>(responseContent);
+                }
+                catch (Exception ex)
+                {
+                    Debug.WriteLine($"Unable to get data: {ex.Message}");
+                    await Shell.Current.DisplayAlert("Greška!", ex.Message, "U redu");
+
+                    return default;
+                }
+            }   
         }
     }
 }
