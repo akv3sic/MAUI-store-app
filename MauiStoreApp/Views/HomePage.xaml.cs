b801747a55cfa156392e5597781fc5565f25d6e1
akv3sic <antekvesic2@gmail.com>
2023-10-07 21:48:58 +0200

    #51 Redesign search bar

--- MauiStoreApp/Views/HomePage.xaml.cs
+++ MauiStoreApp/Views/HomePage.xaml.cs
@@ -12,22 +12,6 @@ public partial class HomePage : ContentPage
 
     private void OnScrolled(object sender, ScrolledEventArgs e)
     {
-        SearchEntry.IsVisible = e.ScrollY < 50;
-		if (e.ScrollY < 50)
-		{
-            SearchBarLayout.Padding = new Thickness(20, 5, 20, 5);
-            Logo.HeightRequest = 40;
-            Logo.Margin = new Thickness(-31, 0, 0, 0);
-            SearchButton.HeightRequest = 22;
-            SearchButton.WidthRequest = 22;
-        }
-        else
-        {
-            SearchBarLayout.Padding = new Thickness(20, 2, 20, 2);
-            Logo.HeightRequest = 35;
-            Logo.Margin = new Thickness(-22, 0, 0, 0);
-            SearchButton.HeightRequest = 20;
-            SearchButton.WidthRequest = 20;
-        }
+		searchBar.IsVisible = e.ScrollY < 90;
     }
 }
